d41a89b292f8b064692ad8ae69589734ad971c72
Totan Sarker <totansarker2@gmail.com>
2024-04-08 19:42:27 +0600

    a20Tk1

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/a',
+        path: '/',
         element: <Home/>,
       },
     ]
