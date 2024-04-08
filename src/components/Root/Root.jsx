bf19ec63f57ecb55da325038fe7855825c38e8d3
Totan Sarker <totansarker2@gmail.com>
2024-04-09 00:21:15 +0600

    a20Tk10

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
 
 const Root = () => {
     return (
         <div className="text-red-500">
             <Outlet></Outlet>
-            
+            <Footer></Footer>
         </div>
     );
 };
