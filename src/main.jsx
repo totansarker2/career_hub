9818f2a044b06503f75a0c4bc9e58c81f9554532
Totan Sarker <totansarker2@gmail.com>
2024-04-09 00:00:19 +0600

    a20Tk1

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import App from './App.jsx'
-import './index.css'
+import "./index.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './components/Home/Home';
-import Root from './components/Root/Root';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Root from "./components/Root/Root";
 import ErrorPage from "./error-page";
-
-
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root/>,
+    element: <Root></Root>,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
-        element: <Root/>,
-        children:[
-          {
-            path:'/',
-            element:<Home></Home>
-          }
-        ]
+        path: "/",
+        element: <Home></Home>,
       },
-    ]
+    ],
   },
 ]);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
