030024fedc843b06db62ceae1ef0d49d2ad7f9dc
Totan Sarker <totansarker2@gmail.com>
2024-04-09 11:06:09 +0600

    a20Tk2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -51,25 +51,7 @@ const Header = () => {
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
+          
         </ul>
       </div>
       <div className="navbar-end">
