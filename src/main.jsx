fb6490a022b8b40a9aa8a8a4ba17648a96cd7584
Totan Sarker <totansarker2@gmail.com>
2024-04-09 11:23:59 +0600

    a20Tk7

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Root from "./components/Root/Root";
+import Jobs from "./components/Jobs/Jobs";
 import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
