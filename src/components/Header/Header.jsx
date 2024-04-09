541522af01e43887a9ada06e63671d57b3275063
Totan Sarker <totansarker2@gmail.com>
2024-04-09 11:06:18 +0600

    a20Tk3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -51,7 +51,7 @@ const Header = () => {
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          
+          {links}
         </ul>
       </div>
       <div className="navbar-end">
