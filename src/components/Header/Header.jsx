d3f04b5f7b36d5bbc049de2157aa2914073a6633
Totan Sarker <totansarker2@gmail.com>
2024-04-09 11:06:25 +0600

    a20Tk4

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,22 +2,22 @@ const Header = () => {
   const links = (
     <>
       <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+        <a>Item 1</a>
+      </li>
+      <li>
+        <a>Parent</a>
+        <ul className="p-2">
+          <li>
+            <a>Submenu 1</a>
+          </li>
+          <li>
+            <a>Submenu 2</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a>Item 3</a>
+      </li>
     </>
   );
   return (
@@ -44,15 +44,13 @@ const Header = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-            
+            {links}
           </ul>
         </div>
         <a className="btn btn-ghost text-xl">daisyUI</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          {links}
-        </ul>
+        <ul className="menu menu-horizontal px-1">{links}</ul>
       </div>
       <div className="navbar-end">
         <a className="btn">Button</a>
