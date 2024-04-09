d2bc595a00df458bdbb368f734a454c0a8d184f5
Totan Sarker <totansarker2@gmail.com>
2024-04-09 16:14:52 +0600

    a20Tk10

--- src/components/CatagoryList/CatagoryList.jsx
+++ src/components/CatagoryList/CatagoryList.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CatagoryList = () => {
     return (
         <div>
-            <h2 className=''></h2>
+            <h2 className='text-6xl text center'></h2>
         </div>
     );
 };
