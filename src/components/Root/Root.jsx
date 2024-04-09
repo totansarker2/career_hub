21ab21c543dc722693a9a72bf53da83077083876
Totan Sarker <totansarker2@gmail.com>
2024-04-09 10:54:43 +0600

    a20Tk0

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -4,7 +4,7 @@ import Footer from "../Footer/Footer";
 
 const Root = () => {
     return (
-        <div className="text-red-500">
+        <div>
             <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
